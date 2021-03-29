777e1a1566f98f10c4bf7f65a8dfe409faccbaef
maxchen0102 <62057890+maxchen0102@users.noreply.github.com>
2021-03-29 23:51:32 +0800

    f

---
+++ link_1/刪除節點.c
@@ -0,0 +1,29 @@
+typedef struct node
+{
+
+    int data;
+    struct node *next;
+} data;
+
+data *delete (struct node *head, struct node *position)
+{
+    if (head == NULL)
+    {
+        printf("nothing ");
+        return NULL;
+    }
+    if (head = position)
+    {
+        head = head->next;
+    }
+    else
+    {
+        while (head->next != position)
+        {
+            head = head->next;
+        }
+        head->next = position->next;
+    }
+    free(position);
+    return head;
+}
