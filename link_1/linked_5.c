d5914ea9a1bf065e27c38478a93d8b386be0ada6
maxchen0102 <62057890+maxchen0102@users.noreply.github.com>
2021-03-29 20:06:53 +0800

    印出串列資料函式

---
+++ link_1/linked_5.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+} data;
+
+//data *listA;
+
+void show_data(data *);
+
+int main()
+{
+    data *previous, *current, *first;
+
+    printf("enter the number of linked");
+    int num;
+    scanf("%d", &num);
+
+    for (int i = 0; i < num; i++)
+    {
+        current = (data *)malloc(sizeof(data));
+        printf("enter the data ");
+        scanf("%d", &current->data);
+
+        if (i == 0)
+        {
+            first = current;
+        }
+        else
+        {
+            previous->next = current;
+        }
+        current->next = NULL;
+        previous = current;
+    }
+
+    //show data
+    show_data(first);
+
+    printf("done\n");
+    return 0;
+}
+
+void show_data(data *p)
+{
+    while (p != 0)
+    {
+        printf("address=%p  ", p);
+        printf("data=%d  ", p->data);
+        printf("next=%p  ", p->next);
+        printf("\n");
+        p = p->next;
+    }
+}
