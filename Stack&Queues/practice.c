2bfff1b740fa380b93a50e3843af2bed70fed3d4
maxchen0102 <62057890+maxchen0102@users.noreply.github.com>
2021-03-31 10:51:58 +0800

    .

---
+++ Stack&Queues/practice.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 5
+
+typedef struct tagStack
+{
+    int item[MAX];
+    int top = 1;
+
+} Stack;
+
+Stack stk;
+
+int main()
+{
+
+    return 0;
+}
